721c2480b39191eb4bab427d37e26ea8c98681e7
Adhanna Hiyas <mth636@uregina.ca>
2020-10-22 15:20:39 -0600

    create files

---
+++ eventlistener.js
@@ -0,0 +1,5 @@
+document.getElementById("insert").addEventListener("click", inserttext);
+
+document.getElementById("test").addEventListener("click", strike);
+
+document.getElementById("remove").addEventListener("click", deleteCheckBox);
